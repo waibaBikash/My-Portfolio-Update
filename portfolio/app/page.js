858414eca2d73868ec216f255e6ef39bf7b0db8e
waibaBikash <kavrelibikash@gmail.com>
2025-01-21 22:51:58 +1100

    create navbar

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,7 +1,12 @@
+'use client'
+
+import Navbar from "@/components/Navbar";
 
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <Navbar/>
+    </>
   );
 }
--- portfolio/tailwind.config.mjs
+++ portfolio/tailwind.config.mjs
@@ -8,9 +8,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F',
       },
+      fontFamily: {
+         Outfit: ["Outfit", "sans-serif"],
+         Ovo: ["Ovo", "serif"]
+      }
     },
   },
   plugins: [],
