69092d341bf723961541428265fc4bf89b02bf21
waibaBikash <kavrelibikash@gmail.com>
2025-01-22 22:45:47 +1100

    mobile menu sidebar functionality

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
+
+  const sideMenuRef = useRef();
+  const openMenu = ()=>{
+     sideMenuRef.current.style.transform = 'translateX(-16rem)'
+  }
+  const closeMenu = ()=>{
+     sideMenuRef.current.style.transform = 'translateX(16rem)'
+  }
   return (
     <>
      <div className='fixed top-0 right-0 w-11/12 z-10 translate-y-[-80%] '>
@@ -25,10 +33,22 @@ const Navbar = () => {
             </button>
            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo '>Contact<Image src={assets.arrow_icon} className='w-3' alt=''/></a>
 
-           <button className='block md:hidden ml-3'>
+           <button className='block md:hidden ml-3' onClick={openMenu}>
              <Image src={assets.menu_black} alt='' className='w-6'/>
            </button>
          </div>
+         {/* Mobile Menu */}
+          <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+
+            <div className='absolute right-6 top-6' onClick={closeMenu}>
+               <Image src={assets.close_black} alt=''/>
+            </div>
+          <li><a className='font-Ovo'  onClick={closeMenu} href="#top">Home</a></li>
+           <li><a className='font-Ovo' onClick={closeMenu}  href="#about">About me</a></li>
+           <li><a className='font-Ovo' onClick={closeMenu}  href="#services">Services</a></li>
+           <li><a className='font-Ovo' onClick={closeMenu}  href="#work">My Work</a></li>
+           <li><a className='font-Ovo' onClick={closeMenu}  href="#contact">Contact me</a></li>
+          </ul>
     </nav>
     </>
   )
