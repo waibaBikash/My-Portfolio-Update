990ef4b6b231a9b48047e14fbc9f2649ee0a0dbd
waibaBikash <kavrelibikash@gmail.com>
2025-01-22 23:18:37 +1100

    about me page

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,5 +1,6 @@
 'use client'
 
+import About from "@/components/About";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 
@@ -9,6 +10,7 @@ export default function Home() {
     <>
     <Navbar/>
     <Header/>
+    <About/>
     </>
   );
 }
