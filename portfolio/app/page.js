6c7fb005b20cfe34fc37cebd494a3415871add4b
waibaBikash <kavrelibikash@gmail.com>
2025-01-22 23:11:15 +1100

    create a header

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,5 +1,6 @@
 'use client'
 
+import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <>
     <Navbar/>
+    <Header/>
     </>
   );
 }
