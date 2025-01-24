ab6f909b9677c104a97b8c62a12469e6f98a2948
waibaBikash <kavrelibikash@gmail.com>
2025-01-24 22:04:37 +1100

    save change

--- portfolio/components/About.jsx
+++ portfolio/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
         <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
-        <h2 className='text-center text-5xl font-Ovo'>About me</h2>
+        <h2 className='text-center text-5xl font-Ovo'>About Me</h2>
 
          <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
             <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
