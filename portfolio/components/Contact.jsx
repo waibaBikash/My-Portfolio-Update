945efca0458d3fabdf7df10c96194d57a06c7334
waibaBikash <kavrelibikash@gmail.com>
2025-01-25 09:48:43 +1100

    complete contat form functionlity

--- portfolio/components/Contact.jsx
+++ portfolio/components/Contact.jsx
@@ -1,8 +1,33 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "bed04237-5dbf-4551-981d-bab8dd3e0f7d");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <div 
       id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
@@ -14,16 +39,18 @@ const Contact = () => {
         commnets, or feedback, please use the from below.
         </p>
 
-      <form className='max-w-2xl mx-auto'>
+      <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
          <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
            <input 
-            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' 
+            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
+            name='name' 
             type="text" 
             placeholder='Enter your name' 
             required 
             />
            <input 
            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
+            name='email'
             type="email" 
             placeholder='Enter your email' 
             required
@@ -31,6 +58,7 @@ const Contact = () => {
          </div>
           <textarea
            className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' 
+           name='message'
            rows='6'
            placeholder='Enter your message'
            required>
@@ -43,7 +71,7 @@ const Contact = () => {
             alt='' className='w-4'/>
           </button>
 
-          <p className='mt-4'>Sending....</p>
+          <p className='mt-4'>{result}</p>
       </form>
     </div>
   )
