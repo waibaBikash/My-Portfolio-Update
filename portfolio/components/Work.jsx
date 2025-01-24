bd898dc02857a1866fed411b85832045cbef6dde
waibaBikash <kavrelibikash@gmail.com>
2025-01-24 22:38:16 +1100

    complete work section

--- portfolio/components/Work.jsx
+++ portfolio/components/Work.jsx
@@ -1,3 +1,5 @@
+import { assets, workData } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Work = () => {
@@ -6,6 +8,26 @@ const Work = () => {
       <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
       <h2 className='text-center text-5xl font-Ovo'>My Latest Work</h2>
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I am a frontend developer from Sydney, Australia with 4 years of experience in multiple companies like Microsot, Telsa and Apple.</p>
+
+      <div className='grid grid-cols-auto my-10 gap-5'>
+         {workData.map((project, index)=>(
+           <div className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' key={index} style={{backgroundImage : `url(${project.bgImage})`}}>
+
+            <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center duration-500 group-hover:bottom-7'>
+            <div>
+                 <h2 className='font-semibold'>{project.title}</h2>
+                 <p className='text-sm text-gray-700'>{project.description}</p>
+              </div>
+               <div className='border rounded-full  border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
+                 <Image src={assets.send_icon} alt='' className='w-5' />
+               </div>
+            </div>
+
+           </div>
+         ))}
+      </div>
+       
+       <a className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500' href="">Show more <Image src={assets.right_arrow_bold} alt='right arrow' className='w-4'/></a>
     </div>
   )
 }
