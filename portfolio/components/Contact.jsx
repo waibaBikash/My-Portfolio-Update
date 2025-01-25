690cd8d68e9a54e1fdcfe9cc04479455aa16ffbb
waibaBikash <kavrelibikash@gmail.com>
2025-01-26 08:31:45 +1100

    dark mode in form

--- portfolio/components/Contact.jsx
+++ portfolio/components/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
   };
   return (
     <div 
-      id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto]'>
+      id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'>
       <h4 className='text-center mb-2 text-lg font-Ovo'>Connect with me</h4>
       <h2 className='text-center text-5xl font-Ovo'>Get in touch</h2>
       <p 
@@ -42,14 +42,14 @@ const Contact = () => {
       <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
          <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
            <input 
-            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
+            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90'
             name='name' 
             type="text" 
             placeholder='Enter your name' 
             required 
             />
            <input 
-           className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
+           className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90'
             name='email'
             type="email" 
             placeholder='Enter your email' 
@@ -57,7 +57,7 @@ const Contact = () => {
              />
          </div>
           <textarea
-           className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' 
+           className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' 
            name='message'
            rows='6'
            placeholder='Enter your message'
@@ -65,7 +65,7 @@ const Contact = () => {
           </textarea>
 
           <button 
-          className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500' 
+          className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover' 
           type='submit'>
             Submit now <Image src={assets.right_arrow_white} 
             alt='' className='w-4'/>
