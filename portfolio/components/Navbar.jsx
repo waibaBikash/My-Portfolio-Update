a13dc4b9a0c9c73e75eed27f6f4b9aa3660fd542
waibaBikash <kavrelibikash@gmail.com>
2025-01-25 23:18:44 +1100

    changes made

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -51,11 +51,21 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
                className='w-6'
                />
             </button>
-           <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50 '>Contact
-           <Image src={ isDarkMode ? assets.arrow_icon_dark :assets.arrow_icon} className='w-3' alt=''/></a>
+           <a 
+            href="#contact"
+            className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50 '>Contact
+           <Image 
+            src={ isDarkMode ? assets.arrow_icon_dark :assets.arrow_icon}
+             className='w-3'
+              alt=''/>
+              </a>
 
            <button className='block md:hidden ml-3' onClick={openMenu}>
-             <Image src={ isDarkMode ? assets.menu_white :assets.menu_black} alt='' className='w-6'/>
+             <Image 
+              src={ isDarkMode ? assets.menu_white :assets.menu_black}
+               alt=''
+                className='w-6'
+                />
            </button>
          </div>
          {/* Mobile Menu */}
