23ef8f9454b51ed419a2fb04944760ec7ff290a9
waibaBikash <kavrelibikash@gmail.com>
2025-01-26 08:22:45 +1100

    dark mode in work

--- portfolio/components/Work.jsx
+++ portfolio/components/Work.jsx
@@ -2,14 +2,14 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Work = () => {
+const Work = ({isDarkMode}) => {
   return (
     <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
       <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
       <h2 className='text-center text-5xl font-Ovo'>My Latest Work</h2>
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>Welcome to my web development portfolio! Wxplore a collection of porjets showcasing my expertise in fornt-end development.</p>
 
-      <div className='grid grid-cols-auto my-10 gap-5'>
+      <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
          {workData.map((project, index)=>(
            <div className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' key={index} style={{backgroundImage : `url(${project.bgImage})`}}>
 
@@ -27,7 +27,7 @@ const Work = () => {
          ))}
       </div>
        
-       <a className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500' href="">Show more <Image src={assets.right_arrow_bold} alt='right arrow' className='w-4'/></a>
+       <a className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover' href="">Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark :assets.right_arrow_bold} alt='right arrow' className='w-4'/></a>
     </div>
   )
 }
