a5f44dbf36c35c4c71b9675d889f5d341023aaee
waibaBikash <kavrelibikash@gmail.com>
2025-01-25 23:16:48 +1100

    save change

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -26,9 +26,14 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
      <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden '>
         <Image src={assets.header_bg_color} alt='' className='w-full'/>
      </div>
-    <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : " "}`}>
+    <nav 
+      className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : " "}`}>
        <a href="#top">
-          <Image src={ isDarkMode ? assets.logo_dark :assets.logo} className='w-28 cursor-pointer mr-14' alt=''/>
+          <Image 
+           src={ isDarkMode ? assets.logo_dark :assets.logo} 
+           className='w-28 cursor-pointer mr-14' 
+           alt=''
+           />
        </a>
         <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? " " : " bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"}`}>
            <li><a className='font-Ovo' href="#top">Home</a></li>
