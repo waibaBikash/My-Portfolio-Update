c3d916c21b4735fe196bfa54cf1e5d1bcb3bf927
waibaBikash <kavrelibikash@gmail.com>
2025-01-26 19:55:50 +1100

    animation in work

--- portfolio/components/Work.jsx
+++ portfolio/components/Work.jsx
@@ -1,17 +1,41 @@
 import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Work = ({isDarkMode}) => {
   return (
-    <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
-      <h2 className='text-center text-5xl font-Ovo'>My Latest Work</h2>
-      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>Welcome to my web development portfolio! Wxplore a collection of porjets showcasing my expertise in fornt-end development.</p>
+    <motion.div
+    initial={{y: -20, opacity: 0}}
+    whileInView={{y: 0, opacity: 1}}
+    transition={{delay: 0.3, duration: 0.5}}
+    id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
+      <motion.h4
+      initial={{opacity: 0}}
+      whileInView={{opacity: 1}}
+      transition={{duration: 0.8, delay: 1}}
+      className='text-center mb-2 text-lg font-Ovo'>My Portfolio</motion.h4>
+      <motion.h2
+      initial={{y: -20, opacity: 0}}
+      whileInView={{y: 0, opacity: 1}}
+      transition={{delay: 0.5, duration: 0.5}}
+      className='text-center text-5xl font-Ovo'>My Latest Work</motion.h2>
+      <motion.p
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{delay: 0.7, duration: 0.5}}
+      className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>Welcome to my web development portfolio! Wxplore a collection of porjets showcasing my expertise in fornt-end development.</motion.p>
 
-      <div className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
+      <motion.div
+       initial={{ opacity: 0}}
+       whileInView={{ opacity: 1}}
+       transition={{delay: 0.9, duration: 0.6}}
+      className='grid grid-cols-auto my-10 gap-5 dark:text-black'>
          {workData.map((project, index)=>(
-           <div className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' key={index} style={{backgroundImage : `url(${project.bgImage})`}}>
+           <motion.div
+           whileHover={{scale: 1.05}}
+           transition={{duration: 0.3}}
+           className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group' key={index} style={{backgroundImage : `url(${project.bgImage})`}}>
 
             <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center duration-500 group-hover:bottom-7'>
             <div>
@@ -23,12 +47,16 @@ const Work = ({isDarkMode}) => {
                </div>
             </div>
 
-           </div>
+           </motion.div>
          ))}
-      </div>
+      </motion.div>
        
-       <a className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover' href="">Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark :assets.right_arrow_bold} alt='right arrow' className='w-4'/></a>
-    </div>
+       <motion.a
+        initial={{ opacity: 0}}
+        whileInView={{ opacity: 1}}
+        transition={{delay: 1.1, duration: 0.5}}
+       className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover' href="">Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark :assets.right_arrow_bold} alt='right arrow' className='w-4'/></motion.a>
+    </motion.div>
   )
 }
 
