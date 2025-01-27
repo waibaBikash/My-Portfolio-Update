7e0ce86c9db51abc8f765c144e0109231c213df4
waibaBikash <kavrelibikash@gmail.com>
2025-01-27 19:56:06 +1100

    save change

--- portfolio/components/Footer.jsx
+++ portfolio/components/Footer.jsx
@@ -6,13 +6,22 @@ const Footer = ({isDarkMode}) => {
   return (
     <div className='mt-20'>
        <div className='text-center'>
-         <Image src={isDarkMode ? assets.logo_dark :assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+         <Image 
+          src={isDarkMode ? assets.logo_dark :assets.logo}
+           alt='' 
+           className='w-36 mx-auto mb-2'
+           />
           <div className='w-max flex items-center gap-2 mx-auto'>
-          <Image src={isDarkMode ? assets.mail_icon_dark :assets.mail_icon} alt='' className='w-6'/> myportfolio@gmail.com
+          <Image 
+          src={isDarkMode ? assets.mail_icon_dark :assets.mail_icon}
+           alt='' 
+           className='w-6'/> 
+           myportfolio@gmail.com
           </div>
        </div>
 
-        <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] my-12 py-6'>
+        <div 
+          className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] my-12 py-6'>
            <p>© 2025 Waiba. All rights reserved.</p>
             <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
                <li><a target='_blank' href="https://github.com/myportfolio">GitHub</a></li>
@@ -24,4 +33,4 @@ const Footer = ({isDarkMode}) => {
   )
 }
 
-export default Footer
+export default Footer;
