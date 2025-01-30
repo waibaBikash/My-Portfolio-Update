81f650656cc5997ecf9cb0ab6f163bcfefed461f
waibaBikash <kavrelibikash@gmail.com>
2025-01-30 22:08:36 +1100

    changes made

--- portfolio/components/Header.jsx
+++ portfolio/components/Header.jsx
@@ -1,7 +1,7 @@
-import { assets } from '@/assets/assets'
-import Image from 'next/image'
-import React from 'react'
-import { motion } from "motion/react"
+import { assets } from '@/assets/assets';
+import Image from 'next/image';
+import React from 'react';
+import { motion } from "motion/react";
 
 const Header = () => {
   return (
