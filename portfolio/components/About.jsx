7c46ad59d5034e1eafc562bc66e80309b910ba38
waibaBikash <kavrelibikash@gmail.com>
2025-01-31 21:00:45 +1100

    save change

--- portfolio/components/About.jsx
+++ portfolio/components/About.jsx
@@ -75,7 +75,9 @@ const About = ({isDarkMode}) => {
                   initial={{y: 20, opacity: 0}}
                   whileInView={{y: 0, opacity: 1}}
                   transition={{delay: 1.3, duration: 0.5}}
-                  className='my-6 text-gray-700 font-Ovo dark:text-white/80'>Tools I use</motion.h4>
+                  className='my-6 text-gray-700 font-Ovo dark:text-white/80'>
+                   Tools I use
+                   </motion.h4>
                    <motion.ul
                    initial={{opacity: 0}}
                    whileInView={{opacity: 1}}
