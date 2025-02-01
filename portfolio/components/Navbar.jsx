25be1b6c82043fb5daeab150009108bca7373169
waibaBikash <kavrelibikash@gmail.com>
2025-02-01 21:58:20 +1100

    commit change

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -30,7 +30,9 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
       className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : " "}`}>
        <a href="#top">
           <Image 
-           src={ isDarkMode ? assets.logo_dark :assets.logo} 
+           src={ isDarkMode 
+            ? assets.logo_dark 
+            :assets.logo} 
            className='w-28 cursor-pointer mr-14' 
            alt=''
            />
