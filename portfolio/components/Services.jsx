4498a0d3b07899a54640fc27fad0fbb808d83323
waibaBikash <kavrelibikash@gmail.com>
2025-02-01 21:59:24 +1100

    save change

--- portfolio/components/Services.jsx
+++ portfolio/components/Services.jsx
@@ -14,12 +14,16 @@ const Services = () => {
       initial ={{y: -20, opacity: 0}}
       whileInView={{y: 0, opacity: 1}}
       transition={{delay: 0.3, duration: 0.5}}
-      className='text-center mb-2 text-lg font-Ovo'>What I Offer</motion.h4>
+      className='text-center mb-2 text-lg font-Ovo'>
+      What I Offer
+      </motion.h4>
       <motion.h2 
       initial ={{y: -20, opacity: 0}}
       whileInView={{y: 0, opacity: 1}}
       transition={{delay: 0.5, duration: 0.5}}
-      className='text-center text-5xl font-Ovo'>My Services</motion.h2>
+      className='text-center text-5xl font-Ovo'>
+      My Services
+      </motion.h2>
       <motion.p
       initial ={{ opacity: 0}}
       whileInView={{ opacity: 1}}
