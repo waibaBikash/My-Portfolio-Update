1042d8ab3eae92d36970f8d0467156a130d507fc
waibaBikash <kavrelibikash@gmail.com>
2025-02-05 22:02:35 +1100

    changes made

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -38,7 +38,8 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
            />
        </a>
         <ul 
-         className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? " " : " bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"}`}>
+         className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 
+         ${isScroll ? " " : " bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"}`}>
            <li><a className='font-Ovo' href="#top">Home</a></li>
            <li><a className='font-Ovo' href="#about">About Me</a></li>
            <li><a className='font-Ovo' href="#services">Services</a></li>
